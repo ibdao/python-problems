5d039a87c96bb8ab854a888cb5afddc9a47e8f2c
Ian Dao <ibdao921@gmail.com>
2022-06-07 10:11:15 -0700

    fixed palindrome

--- 09_is_palindrome.py
+++ 09_is_palindrome.py
@@ -21,8 +21,6 @@ def is_palindrome(phrase):
         >>> is_palindrome('Noon')
         True
     """
-    if (phrase.find(" ") >= 0):
-        return phrase.join(" ").lower() == phrase.join(" ").lower()[::-1]
-    else:
-        return phrase.lower() == phrase.lower()[::-1]
+    return phrase.replace(" " , "").lower() == phrase.replace(" " , "").lower()[::-1]
+    
       
